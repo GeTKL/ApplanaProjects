3b85c2ce51410f65f9da497771ad7d6bf76af20c
GeT <Artem-klevtsov@mail.ru>
2025-04-03 22:37:59 +0300

    add 2nd exercise

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import exercise.*;
 
 public class Main {
     public static void main(String[] args) {
-        firstExercise firstExercise = new firstExercise();
-        firstExercise.helloWorld();
+        secExercise.datasTypes();
     }
 }
 
---
+++ src/exercise/secExercise.java
@@ -0,0 +1,10 @@
+package exercise;
+
+public class secExercise {
+    public static void datasTypes(){
+        double decVariable = 5.6342;
+        long fullVariable = 321452L;
+
+        System.out.printf("Переменная с плавающей точкой %f, а это переменная с целым значением %d", decVariable, fullVariable);
+    }
+}
