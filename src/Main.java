fd98f4ee486b53c2a4db6040104eeccfae37eb15
GeT <Artem-klevtsov@mail.ru>
2025-04-06 14:42:41 +0300

    add 7th exercise

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import exercise.*;
 
 public class Main {
     public static void main(String[] args) {
-        fourthExercise.operators(); // 4 задание
-        // fifthExercise.calculator(); // 5 задание
+        sevenExercise.compareArray();
     }
 }
 
---
+++ src/exercise/sevenExercise.java
@@ -0,0 +1,37 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class sevenExercise {
+    public static void compareArray(){
+        final int x = 12;
+        final int y = 5;
+        final int z = 3;
+
+        System.out.println("Введите размер массива: ");
+        Scanner sc = new Scanner(System.in);
+        int arrayLength = sc.nextInt();
+
+        System.out.println("Введите числа через пробел: ");
+        sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+
+        int[] array = new int[arrayLength];
+
+        if (input.length > arrayLength)
+        {
+            System.out.println("Количество чисел превышает длину массива, повторите попытку.");
+            System.exit(0);
+        }
+
+        int counter = 0;
+        while (counter < arrayLength) {
+            array[counter] = Integer.parseInt(input[counter]);
+            if (array[counter] == x || array[counter] == y || array[counter] == z) {
+                System.out.println("Значение " + array[counter] + " имеется в константах");
+                break;
+            }
+            counter++;
+        }
+    }
+}
