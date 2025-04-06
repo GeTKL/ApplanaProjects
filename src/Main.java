972f4d8c014c725867a471ca8905d7d56a7f872b
Клевцов Артем Александрович <aklevtsov@ibs.ru>
2025-04-06 22:31:34 +0300

    Completing first Exam exercise.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import exercise.*;
 
 public class Main {
     public static void main(String[] args) {
-        tenthExercise.matrix(); // 10 задание
+        firstExamExercise.converterValute(); // 1 Итоговое задание
+        // tenthExercise.matrix(); // 10 задание
         // ninthExercise.averageCompis(); // 9 задание
         //eighthExercise.oddNumbers(); // 8 задание
         // seventhExercise.compareArray(); // 7 задание
---
+++ src/exercise/firstExamExercise.java
@@ -0,0 +1,29 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class firstExamExercise {
+    static Scanner in = new Scanner(System.in);
+    public static void converterValute(){
+
+        System.out.println("Введите курс доллара: ");
+        in = new Scanner(System.in);
+        check();
+        double dollar = in.nextDouble();
+
+        System.out.println("Введите количество рублей: ");
+        in = new Scanner(System.in);
+        check();
+        double rubles = in.nextDouble();
+
+        double total = dollar * rubles;
+        System.out.println("Итого: " + String.format("%.2f",total));
+    }
+
+    public static void check(){
+        if(!in.hasNextDouble()){
+            System.out.println("Необходимо ввести число!");
+            System.exit(0);
+        }
+    }
+}
